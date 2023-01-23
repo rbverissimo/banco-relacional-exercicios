29d1806bb4e2fa89436a293f27d30d72e4572632
rbverissimo <rverissimo.dev@gmail.com>
2023-01-23 12:06:53 -0300

    Fazendo consultas usando o Join e a identificação a partir da chave primária de uma tabela que é chave estrangeira de outra. Adição também de um arquivo texto sobre os tipos de Join

--- consulta-join.sql
+++ consulta-join.sql
@@ -1,1 +1,18 @@
-select * from estados e, cidades c
+-- é necessário igualar a PK de e com FK de c; 
+select * from estados e, cidades c where e.id = c.estado_id;
+
+-- resolvendo um bug do VSCode passando um alias para cada uma das colunas
+select 
+      e.nome as Estado, 
+      c.nome as Cidade, 
+      regiao as Região 
+from estados e, cidades c 
+where e.id = c.estado_id; 
+
+-- mesma consulta anterior só que usando o inner join
+select 
+      e.nome as Estado, 
+      c.nome as Cidade, 
+      regiao as Região 
+from estados e
+inner join cidades c on ed.id = c.estado_id;
